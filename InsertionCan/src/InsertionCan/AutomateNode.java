8a182ab0a6dbf74ba81e191ca7534de564645809
3802558 <rosa.mendas@etu.sorbonne-universite.fr>
2021-04-26 01:31:37 +0200

    commit insert

--- InsertionCan/src/InsertionCan/AutomateNode.java
+++ InsertionCan/src/InsertionCan/AutomateNode.java
@@ -30,11 +30,13 @@ import java.util.Hashtable;
 //noeud dans un LeafSet.
 //=========================================================================
 public class AutomateNode {
-	private int num,size,x,y,master;
+	private int num,size,x,y,master,ecart,tailleauto,b;
 	private String name;
 	private PNMLManipulation manip;
 	private PlaceHLAPI principalNode,dhtinfo,estinsere,oKshare,nOshare,req;
 	private TransitionHLAPI firstinsert,moreinsert,associate,asksharing,isinsert;
+	private TransitionHLAPI[] askreq,gestion,accept,refus;
+	private PlaceHLAPI[] placescomm,placescomm2;
 	
 	//=========================================================================
 	//transition1,transition2,transition3Left,transition3Right,transition4Left,
@@ -50,6 +52,18 @@ public class AutomateNode {
 		this.y=y;
 		this.manip=manip;
 
+		ecart = 85;
+		tailleauto = 1000;
+		b = y + 100;
+
+		placescomm = new PlaceHLAPI[size];
+		placescomm2 = new PlaceHLAPI[size];
+		askreq = new TransitionHLAPI[size];
+		gestion = new TransitionHLAPI[size];
+		accept = new TransitionHLAPI[size];
+		refus = new TransitionHLAPI[size];
+
+
 	}
 	
 	//=========================================================================
@@ -64,12 +78,6 @@ public class AutomateNode {
 
 
 	public void InsertInEmptyCan(){
-
-		int absdep = 250;
-		int ecart = 75;
-		int tailleauto = size*absdep;
-		
-
 
 		//Noeud à insérer
 		manip.place(name+"AInserer",x + tailleauto/2, y,CSS2Color.BLACK,true);
@@ -107,18 +115,13 @@ public class AutomateNode {
 		associate = manip.getTransition();
 		manip.arc(true,manip.getPlace(),associate);
 
-		manip.place(name+"IsInserted",x + tailleauto/4, b+9*ecart ,CSS2Color.BLACK,false);
+		manip.place(name+"IsInserted",x + tailleauto/4, b+12*ecart ,CSS2Color.BLACK,false);
 		estinsere = manip.getPlace();
 		manip.arc(false,estinsere,associate);
 
 	}
 
 	public void insertInNotEmptyCan(){
-
-		int absdep = 250;
-		int ecart = 75;
-		int tailleauto = size*absdep;
-		int b = y + 100;
 
 		//construction branche Nième insertion:
 
@@ -148,22 +151,22 @@ public class AutomateNode {
 		asksharing = manip.getTransition();
 		manip.arc(true,manip.getPlace(),asksharing);
 
-		manip.place(name+"Request",x+100+3*tailleauto/4,b+9*ecart,CSS2Color.RED,false);
+		manip.place(name+"Request",x + 3*tailleauto/4+150,b+8*ecart,CSS2Color.RED,false);
 		req = manip.getPlace();
 		manip.arc(false,manip.getPlace(),manip.getTransition());
 
-		manip.place(name+"WaitAnswer",x-ecart+3*tailleauto/4, b+8*ecart,CSS2Color.BLACK,false);
+		manip.place(name+"WaitAnswer",x + 3*tailleauto/4, b+9*ecart,CSS2Color.BLACK,false);
 		manip.arc(false,manip.getPlace(),manip.getTransition());
 		
-		manip.transition(name+"InsertNode",x-ecart+3*tailleauto/4,b+9*ecart,CSS2Color.BLACK);
+		manip.transition(name+"InsertNode",x+3*tailleauto/4,b+12*ecart,CSS2Color.BLACK);
 		isinsert = manip.getTransition();
 		manip.arc(true,manip.getPlace(),isinsert);
 		manip.arc(false,estinsere,isinsert);
 
-		manip.transition(name+"NotInsertNode",x-2*ecart+3*tailleauto/4,b+8*ecart,CSS2Color.BLACK);
+		manip.transition(name+"NotInsertNode",x-2*ecart+3*tailleauto/4,b+9*ecart,CSS2Color.BLACK);
 		manip.arc(true,manip.getPlace(),manip.getTransition());
 
-		manip.place(name+"OKshare",x-ecart+3*tailleauto/4,b+10*ecart,CSS2Color.ORANGE,false);
+		manip.place(name+"OKshare",x + 3*tailleauto/4,b+13*ecart,CSS2Color.ORANGE,false);
 		oKshare = manip.getPlace();
 		manip.arc(true,oKshare,isinsert);
 		manip.place(name+"NOshare",x-2*ecart+3*tailleauto/4,b+10*ecart,CSS2Color.ORANGE,false);
@@ -176,8 +179,66 @@ public class AutomateNode {
 		manip.arc(true,manip.getPlace(),manip.getTransition());
 		manip.arc(false,dhtinfo,manip.getTransition());
 
+	}
 
 
+	public void communicNodes(){
+
+		//créer autant de transisions que de noeuds - 1
+		for (int j=0; j<size; j++ ) {
+
+			if (j!=num) {
+			
+				manip.transition(name+"AskReq"+j, x+3*tailleauto/4+150, b+8*ecart+(j+1)*100 ,CSS2Color.RED);
+				askreq[j] = manip.getTransition();
+				
+				manip.arc(true,req,manip.getTransition());
+				manip.place(name+"requestsent"+j,x+3*tailleauto/4+200, b+8*ecart+(j+1)*100 ,CSS2Color.RED,false);
+				placescomm[j] = manip.getPlace();
+				
+				manip.arc(false,manip.getPlace(),manip.getTransition());
+				manip.transition(name+"Gestion"+j,x + tailleauto/4, b+15*ecart+(j+1)*150,CSS2Color.BLUE);
+				gestion[j] = manip.getTransition();
+				
+				manip.arc(true,estinsere,manip.getTransition());
+				manip.arc(false,estinsere,manip.getTransition());
+				manip.place(name+"GiveAnswer"+j,x-ecart+tailleauto/4, b+15*ecart+(j+1)*150,CSS2Color.BLUE,false);
+				placescomm2[j] = manip.getPlace();
+				
+				manip.arc(false,manip.getPlace(),manip.getTransition());
+				manip.transition(name+"SendAccept"+j,x-2*ecart+tailleauto/4, b+15*ecart+(j+1)*150,CSS2Color.BLUE);
+				accept[j] = manip.getTransition();
+				
+				manip.arc(true,manip.getPlace(),manip.getTransition());
+				manip.transition(name+"SendRefuse"+j,x-ecart+tailleauto/4, b+15*ecart+(j+1)*150-ecart,CSS2Color.BLUE);
+				refus[j] = manip.getTransition();
+				
+				manip.arc(true,manip.getPlace(),manip.getTransition());
+			}
+
+		}
+
+	}
+
+
+	public PlaceHLAPI getplacescomm(int i){
+		return placescomm[i];
+	}
+
+	public TransitionHLAPI getaccepte(int i){
+		return accept[i];
+	}
+
+	public TransitionHLAPI getaskreq(int i){
+		return askreq[i];
+	}
+
+	public TransitionHLAPI getrefus(int i){
+		return refus[i];
+	}
+
+	public TransitionHLAPI getgestion(int i){
+		return gestion[i];
 	}
 	
 	public PlaceHLAPI getreq(){
--- InsertionCan/src/InsertionCan/CAN.java
+++ InsertionCan/src/InsertionCan/CAN.java
@@ -30,15 +30,12 @@ import fr.lip6.move.pnml.ptnet.hlapi.PetriNetDocHLAPI;
 
 public class CAN{
 	private PNMLManipulation manip;
-	private int x,y,size,num;
+	private int x,y,size;
 	private String name;
 	private PlaceHLAPI cpt,canvide;
 	private TransitionHLAPI trans1,trans2,trans3;
-	private TransitionHLAPI[][] askreq,gestion,accept,refus;
 	private AutomateNode[] automatesNode;
 	private AutomateNode automate;
-	private PlaceHLAPI[][] placescomm,placescomm2;
-	private List<Integer> list;
 
 	public CAN(int size,PNMLManipulation manip) {
 		this.manip=manip;
@@ -51,31 +48,12 @@ public class CAN{
 		int orddep = 400;
 		int tailleauto = size*absdep;
 
-		if (size>2) {
-
-			manip.place("Compteur",size*(tailleauto+500)/2 -500 ,y+1500,CSS2Color.GREEN,false);
-			cpt=manip.getPlace();
-			manip.place("CANVide",size*(tailleauto+500)/2 -500, y+100,CSS2Color.GREEN,true);
-			canvide=manip.getPlace();
-			
-		}
-		else{
-			manip.place("Compteur",size*(tailleauto+500)/2 ,y+1500,CSS2Color.GREEN,false);
-			cpt=manip.getPlace();
-			manip.place("CANVide",size*(tailleauto+500)/2,y+100,CSS2Color.GREEN,true);
-			canvide=manip.getPlace();
-		}
-		
-
-		
+		manip.place("Compteur",size*(tailleauto+500)/2 + 200,y+100,CSS2Color.GREEN,false);
+		cpt=manip.getPlace();
+		manip.place("CANVide",size*(tailleauto+500)/2, y+100,CSS2Color.GREEN,true);
+		canvide=manip.getPlace();
 
 		automatesNode = new AutomateNode[size];
-		placescomm = new PlaceHLAPI[size][size];
-		placescomm2 = new PlaceHLAPI[size][size];
-		askreq = new TransitionHLAPI[size][size];
-		gestion = new TransitionHLAPI[size][size];
-		accept = new TransitionHLAPI[size][size];
-		refus = new TransitionHLAPI[size][size];
 
 	}
 
@@ -87,23 +65,48 @@ public class CAN{
 
 	public void buildAutomatesNodes() {
 
-		int ecart = 200;
 		int absdep = 250;
 		int orddep = 400;
 		int tailleauto = size*absdep;
-		int departauto = ecart;
+		int departauto = 100;
 
 		for (int i=0;i<size; i++) {
-			
+			departauto = (i)*(tailleauto+1000);
 			automatesNode[i] = new AutomateNode(departauto,orddep,i,size,manip);
 			automatesNode[i].buildAutomate();
-			departauto = 500+(i+1)*tailleauto;
+			
 			
 		}
 
+		for (int i=0; i<size ;i++ ) {
+			automatesNode[i].communicNodes();
+		}
+
+
+
+		for (int i=0; i<size; i++) {
+			for (int j=0; j<size; j++) {
+				if (j != i) {
+
+					AutomateNode me = automatesNode[i];
+					AutomateNode autrej = automatesNode[j];
+
+					manip.arc(false,me.getokshare(),autrej.getaccepte(i));
+					manip.arc(false,me.getnoshare(),autrej.getrefus(i));
+
+					manip.arc(true,autrej.getplacescomm(i),me.getgestion(j));
+
+					manip.arc(false,me.getestinsere(),autrej.getaskreq(i));
+					manip.arc(true,me.getestinsere(),autrej.getaskreq(i));
+
+				
+				}
+
+			}
+			
+		}
+					
 	}
-
-
 
 
 	public void buildConnexions() {
@@ -111,50 +114,12 @@ public class CAN{
 		//on créer les automates des noeuds
 		for(int i=0; i<size; i++) {
 
-			/*
-			int abs=0;
-			int ord = 0;
-			if (i%2==0) {
-				if (i<size/2) {
-					automatesNode[i]=new AutomateNode(x-size*200,y-i*500,i,size,manip);
-					abs = x-size*200;
-					ord = y-i*200;
-					
-				}
-				else{
-					automatesNode[i]=new AutomateNode(x-size*200,y+i*500,i,size,manip);
-					abs = x-size*200;
-					ord = y+i*200;
-				}
-			}
-			else{
-				if (i<size/2) {
-					automatesNode[i]=new AutomateNode(x+size*200,y-i*500,i,size,manip);
-					abs = x+size*200;
-					ord = y-i*200;
-					
-				}
-				else{
-					automatesNode[i]=new AutomateNode(x+size*200,y+i*500,i,size,manip);
-					abs = x+size*200;
-					ord = y+i*500;
-				}				
-				
-			}
-			
-			name="Node"+i;
-			automatesNode[i].buildAutomate(); */
-			//créer les places de communication
-
-
 			//relier compteur et transitions : 
 			TransitionHLAPI trans1 = automatesNode[i].getmoreinsert();
 			TransitionHLAPI trans2 = automatesNode[i].getisinsert();
 			TransitionHLAPI trans3 = automatesNode[i].getassociate();
 			TransitionHLAPI trans4 = automatesNode[i].getfirstinsert();
-			PlaceHLAPI estdanscan = automatesNode[i].getestinsere();
-			PlaceHLAPI req = automatesNode[i].getreq();
-
+			
 			manip.arc(true,cpt,trans1);
 			manip.arc(false,cpt,trans1);
 			manip.arc(true,canvide,trans4);
@@ -162,63 +127,6 @@ public class CAN{
 			manip.arc(false,cpt,trans3);
 		}
 	}
-			/*
-			//créer autant de transisions que de noeuds - 1
-			for (int j=0; j<size; j++ ) {
-
-				if (j!=i) {
-				
-					manip.transition(name+"AskReq"+j,abs-450,ord-600+j*100,CSS2Color.RED);
-					askreq[i][j] = manip.getTransition();
-					
-					manip.arc(true,req,manip.getTransition());
-					manip.place(name+"requestsent"+j,abs-400,ord-700+j*100,CSS2Color.RED,false);
-					placescomm[i][j] = manip.getPlace();
-					
-					manip.arc(false,manip.getPlace(),manip.getTransition());
-					manip.transition(name+"Gestion"+j,abs+300+j*100,ord-400+j*100,CSS2Color.BLUE);
-					gestion[i][j] = manip.getTransition();
-					
-					manip.arc(true,estdanscan,manip.getTransition());
-					manip.arc(false,estdanscan,manip.getTransition());
-					manip.place(name+"GiveAnswer"+j,abs+400+j*100,ord-500+j*100,CSS2Color.BLUE,false);
-					placescomm2[i][j] = manip.getPlace();
-					
-					manip.arc(false,manip.getPlace(),manip.getTransition());
-					manip.transition(name+"SendAccept"+j,abs+300+j*100,ord-600+j*100,CSS2Color.BLUE);
-					accept[i][j] = manip.getTransition();
-					
-					manip.arc(true,manip.getPlace(),manip.getTransition());
-					manip.transition(name+"SendRefuse"+j,abs+300+j*100,ord-500+j*100,CSS2Color.BLUE);
-					refus[i][j] = manip.getTransition();
-					
-					manip.arc(true,manip.getPlace(),manip.getTransition());
-				}
-
-			}
-		}
-
-		//on crée les connections entre les automates
 		
-		for(int i=0;i<size;i++) {
-			automate = automatesNode[i];
-
-			for (int j=0;j<size ;j++ ) {
-				if (j != i) {
-
-					manip.arc(false,automatesNode[j].getokshare(),accept[i][j]);
-					manip.arc(false,automatesNode[j].getnoshare(),refus[i][j]);
-					manip.arc(true,automatesNode[j].getestinsere(),askreq[i][j]);
-					manip.arc(false,automatesNode[j].getestinsere(),askreq[i][j]);
-					manip.arc(true,placescomm[j][i],gestion[i][j]);
-
-				}
-				
-			}
-
-		}
-		
-	}
-*/
 
 }
